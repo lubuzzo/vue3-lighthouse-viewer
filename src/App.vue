1a706dc0c99f09a0c21b15013aa3ecad3f8fe7ab
Lucas Buzzo <lcsbzo@gmail.com>
2022-02-10 09:20:30 -0300

    Added simple base as example

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import VueLighthouseViewer from 'vue-lighthouse-viewer';
+
+export default {
+  data() {
+    return {
+      json: '',
+    };
+  },
+  mounted() {
+    this.getReport();
+  },
+  methods: {
+    async getReport() {
+      import('../report.json').then(data => {
+        console.log(data)
+        this.json = data;
+      })
+    },
+  },
+};
+</script>
+
+<template>
+  <VueLighthouseViewer :json="json" />
+</template>
